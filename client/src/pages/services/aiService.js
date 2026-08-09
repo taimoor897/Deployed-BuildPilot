8c9656066559947d26b537cd2f4e43077d0a8dc2
taimoor897 <tsbhatti789@gmail.com>
2026-08-09 16:00:03 +0500

    Update aiService.js

--- client/src/pages/services/aiService.js
+++ client/src/pages/services/aiService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://localhost:5000/api/ai";
+const API = `${import.meta.env.VITE_API_URL}/api/ai`;
 
 export async function sendMessage(message) {
   const { data } = await axios.post(`${API}/chat`, {
